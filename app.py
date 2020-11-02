5de47878541a03155511fbb24b42da60d9996452
surajs <3ssalunke>
2020-11-03 01:04:19 +0530

    modified app.

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
+from db import db
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
     app.run(port=5000, debug=True)
